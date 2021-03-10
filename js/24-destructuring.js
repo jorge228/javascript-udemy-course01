eda9949bc1889ea99ecf578291c41e3a4fd296db
jorge <jorgelozano228@gmail.com>
2021-03-10 08:16:15 +0100

    exercises and babel

---
+++ js/24-destructuring.js
@@ -0,0 +1,44 @@
+/* 
+https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Destructuring_assignment
+
+La destructuración es una expresión de JavaScript que hace posible la extracción de datos de arreglos u objetos
+*/
+
+const person = {
+    name: 'Dorian',
+    age: 20,
+    email: 'dorian@gmail.com'
+}
+
+//FORMA TRADICIONAL
+// const name = person.name;
+// const age = person.age;
+// const email = person.email;
+
+//DESTRUCTURING
+// const { name: nombre, age: edad, email } = person;
+// console.log(nombre, edad, email);
+
+// const { name, age, email } = person;
+// console.log(name, age, email);
+
+// const numbers = [1, 2, 3, 4];
+// const [a, b, terceraPosicion] = numbers;
+// console.log(terceraPosicion);
+
+// const printPerson = ({ name }) => {
+//     console.log(name);
+// }
+// printPerson(person);
+// const printPerson = ({ name: nombre }) => {
+//     console.log(nombre);
+// }
+// printPerson(person);
+
+const getUsers = async () => {
+    const { data: users } = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+    console.log(users);
+}
+
+getUsers();
